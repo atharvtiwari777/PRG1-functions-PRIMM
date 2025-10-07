7cc8735ee2c2b79c87d130e65cbdcefbff9bfc05
atharvtiwari777 <atharvtiwari777>
2025-10-07 10:03:26 +0100

    modified temprature converter

--- 1_temperature_converter.py
+++ 1_temperature_converter.py
@@ -2,7 +2,15 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
 
+def fahrenheit_to_celsius(fahrenheit):
+    celcius = (fahrenheit - 32) / 1.8
+    return celcius
+
+
 # Test the function
 print(celsius_to_fahrenheit(0))
 print(celsius_to_fahrenheit(20))
 print(celsius_to_fahrenheit(100))
+print(fahrenheit_to_celsius(32))
+print(fahrenheit_to_celsius(68))
+print(fahrenheit_to_celsius(212))
